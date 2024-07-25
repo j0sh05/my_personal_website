b4734c4d09a0d1c1cf3ce708140abb26860c9e3a
j0sh05 <97497822+j0sh05@users.noreply.github.com>
2024-07-24 21:38:53 -0400

    Added Material Icons, working on inspiration

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,14 @@
 import Link from "next/link";
+import MenuIcon from '@mui/icons-material/Menu';
 
 export default function Navbar() {
     return (
-        <nav className="navbar justify-around sticky w-full ">
-            <h1><Link href={"/"}>My Website</Link></h1>
-            <div className="flex gap-4">
+        <nav className="navbar md:justify-around sticky w-full">
+            <h1 className="hidden md:flex md:gap-4"><Link href={"/"}>My Website</Link></h1>
+            <div className="navbar-start md:hidden">
+                <MenuIcon />
+            </div>
+            <div className="hidden md:flex md:gap-4">
                 <Link href="/">Home</Link>
                 <Link href="/about">About</Link>
                 <Link href="/projects">Projects</Link>
